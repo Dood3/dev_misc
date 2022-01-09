49bae55a29d0f1bc07135eacf48ab404d0761269
Dood3 <93183445+Dood3@users.noreply.github.com>
2022-01-09 09:00:33 +0100

    Create make_qr.py

---
+++ make_qr.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+import qrcode
+
+#---------------------------------------------------
+
+if len(sys.argv) <= 2:
+	print("====================================================")
+	print("Create a QR-Code leading to a link as .png")
+	print((sys.argv[0]) +" <link> <outputfile>")
+	print("====================================================")
+	sys.exit(0)
+
+#---------------------------------------------------
+
+qr = qrcode.QRCode(
+	version = 1,
+	error_correction = qrcode.constants.ERROR_CORRECT_H,
+	box_size = 7,
+	border = 4,
+	)
+
+qr.add_data(sys.argv[1])
+qr.make(fit = True)
+
+img = qr.make_image()
+img.save(sys.argv[2] + ".png")
